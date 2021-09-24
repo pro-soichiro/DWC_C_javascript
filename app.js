72f237a83a3159d1a10f773718c37b3da0e357df
pro-soichiro <dr.1987626@gmail.com>
2021-09-24 21:43:29 +0900

    [Update] 関数を使った計算式の出力

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-let answer = 1 + 1 ;
-alert("1+1=" + answer);
-answer = 1 - 1 ;
-alert("1-1=" + answer);
-answer = 1 * 1 ;
-alert("1*1=" + answer);
-answer = 1 / 1 ;
-alert("1÷1=" + answer);
+function math(a,b) {
+  console.log(`${a} + ${b} = `+ (a + b));
+  console.log(`${a} - ${b} = `+ (a - b));
+  console.log(`${a} * ${b} = `+ (a * b));
+  console.log(`${a} ÷ ${b} = `+ (a / b));
+}
 
-// 自己流応用
-
-let a = 4;
-let b = 2;
-// alert(`${a} + ${b} = `+`${a + b}`);
-alert(`${a} + ${b} = `+(a + b));
-alert(`${a} - ${b} = `+(a - b));
-alert(`${a} * ${b} = `+ a * b);
-alert(`${a} / ${b} = `+ a / b);
+math(3,4);
