574247b62d494ac603d4ef510e5541ff5dc6e6cc
pro-soichiro <dr.1987626@gmail.com>
2021-09-25 08:07:07 +0900

    [Update] 5章【jQueryでHTMLのCSS要素を操作しよう】確認問題　解答

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function(){
+  $('.box1').slideDown();
+  setTimeout(function(){
+    $('.box1').css({
+      'background-color':'#0000FF',
+      'height': '100px'
+    });
+  },1000);
+  setTimeout(function(){
+    $('.box1').slideUp();
+  },2000);
 });
