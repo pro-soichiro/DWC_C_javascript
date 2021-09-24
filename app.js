e0df7ee11624a95b309eae3dd1eb83646e0cb28e
pro-soichiro <dr.1987626@gmail.com>
2021-09-24 22:34:37 +0900

    [Finish] 3章 【関数】確認問題 模範回答

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-// じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-
-// じゃんけんの手をランダムに作成する関数を呼び出す
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
 let js_hand = getJShand();
-
-// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
 let judge = winLose(user_hand, js_hand);
 
-// 結果を表示する
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
-
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
+}
 // ランダムでじゃんけんの手を作成する関数
 function getJShand(){
   let js_hand_num = Math.floor( Math.random() * 3 );
@@ -54,17 +55,17 @@ function winLose(user, js){
     } else if(js == "パー"){
       winLoseStr = "あいこ";
     }
-  } else if(user == null) {
-    if (!alert("またチャレンジしてね")){
-      window.location.reload();
-      exit;
-    };
-  } else {
-    if(!alert("グー・チョキ・パーのいずれかを入力してください")){
-      window.location.reload();
-      exit;
-    };
+  // } else if(user == null) {
+  //   if (!alert("またチャレンジしてね")){
+  //     window.location.reload();
+  //     exit;
+  //   };
+  // } else {
+  //   if(!alert("グー・チョキ・パーのいずれかを入力してください")){
+  //     window.location.reload();
+  //     exit;
+  //   };
   }
   return winLoseStr;
 }
-window.location.reload();
+// window.location.reload();
