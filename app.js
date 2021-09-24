66d7b74233c0c2d5b6083ca1015f2b4b6f47cf68
pro-soichiro <dr.1987626@gmail.com>
2021-09-24 18:19:24 +0900

    [Update] 2章 【JavaScriptの基本文法】

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert('Hello World');
+let answer = 1 + 1 ;
+alert("1+1=" + answer);
+answer = 1 - 1 ;
+alert("1-1=" + answer);
+answer = 1 * 1 ;
+alert("1*1=" + answer);
+answer = 1 / 1 ;
+alert("1÷1=" + answer);
+
+// 自己流応用
+
+let a = 4;
+let b = 2;
+// alert(`${a} + ${b} = `+`${a + b}`);
+alert(`${a} + ${b} = `+(a + b));
+alert(`${a} - ${b} = `+(a - b));
+alert(`${a} * ${b} = `+ a * b);
+alert(`${a} / ${b} = `+ a / b);
