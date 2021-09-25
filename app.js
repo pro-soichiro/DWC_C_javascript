4950d409854f66f9277bafa0522412be9b7c37ad
pro-soichiro <dr.1987626@gmail.com>
2021-09-25 09:21:11 +0900

    [Update] 8章 【jQueryのプラグインを導入しよう】

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 $(function(){
-  $('.box1').slideDown();
-  setTimeout(function(){
-    $('.box1').css({
-      'background-color':'#0000FF',
-      'height': '100px'
-    });
-  },1000);
-  setTimeout(function(){
-    $('.box1').slideUp();
-  },2000);
-});
-
-// クリックイベントによって前回の処理が行われるようにする
-$(function (){
-  $('.box2').slideDown();
-  $('.box2').on('click',function(){
-    $('.box2').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
+  $('.box1').on('click', function(){
+    $(this).slideUp();
   });
 });
-
